7a8dd20f420b9622a129c8e1073c2ee17b8ca5a4
yseulpark <yseulpark26@gmail.com>
2026-03-17 20:13:58 +0200

    9.2

---
+++ Module 9/9.2.py
@@ -0,0 +1,20 @@
+class Car:
+    def __init__(self,registration_number, maximum_speed):
+        self.registration_number = registration_number
+        self.maximum_speed = maximum_speed
+        self.current_speed = 0
+        self.travelled_distance = 0
+    def accelerate(self,change):
+        self.current_speed += change
+        if self.current_speed > self.maximum_speed:
+            self.current_speed = self.maximum_speed
+        elif self.current_speed < 0:
+            self.current_speed = 0
+car=Car("ABC-123",142)
+car.accelerate(30)
+car.accelerate(70)
+car.accelerate(50)
+print(f"Current speed of the car is {car.current_speed} km/h.")
+car.accelerate(-200)
+print(f"Final speed of the car is {car.current_speed} km/h.")
+
